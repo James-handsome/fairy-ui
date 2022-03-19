fe5ec30d2a53796c56a5722e0bb24205c4d64c1b
310896024@qq.com <zhanhao>
2022-03-19 21:18:51 +0800

    dev

--- examples/main.js
+++ examples/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-// less
+
 import "../components/css/card.less";
 import Card from "../components/lib/card/index";
 
