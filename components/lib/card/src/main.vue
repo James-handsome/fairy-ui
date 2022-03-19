dcaa8e4972d1c21fe546f9046a6bd0c7f6f2dfef
310896024@qq.com <zhanhao>
2022-03-19 19:51:18 +0800

    dev

--- components/lib/card/src/main.vue
+++ components/lib/card/src/main.vue
@@ -1,10 +1,34 @@
 <template>
-  <div>Card</div>
+  <div class="m-card">
+    <div class="m-card-img">
+      <img :src="imgSrc" alt="img" />
+    </div>
+    <div class="m-card-summary">summary</div>
+    <div class="footer">footer</div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Card",
+  props: {
+    widht: {
+      type: Number,
+      default: 0,
+    },
+    imgSrc: {
+      type: String,
+      default: "",
+    },
+    imgHeight: {
+      type: Number,
+      default: 0,
+    },
+    summary: {
+      type: String,
+      default: "",
+    },
+  },
   data() {
     return {};
   },
