7df2e05bf856e8f30b564bae2c3403162b079a94
310896024@qq.com <zhanhao>
2022-03-19 22:08:31 +0800

    dev

---
+++ components/lib/index.js
@@ -0,0 +1,18 @@
+import Card from "./card";
+
+const components = {
+  Card,
+};
+
+const install = function (Vue) {
+  if (install.installed) return;
+  Object.keys(components).forEach((key) => {
+    Vue.component(components[key].name, components[key]);
+  });
+};
+
+const API = {
+  install,
+};
+
+export default API;
