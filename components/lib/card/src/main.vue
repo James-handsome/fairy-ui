eb4afd13f9d86de4303e1aa08fd6eb57324ea249
310896024@qq.com <zhanhao>
2022-03-19 18:33:24 +0800

    dev

--- components/lib/card/src/main.vue
+++ components/lib/card/src/main.vue
@@ -6,4 +6,4 @@
 export default {};
 </script>
 
-<style lang="less"></style>
+<style></style>
--- examples/main.js
+++ examples/main.js
@@ -3,6 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "../components/css/card.less";
+import Card from "../components/lib/card/src/main.vue";
+
+Vue.use(Card);
+
 Vue.config.productionTip = false;
 
 new Vue({
