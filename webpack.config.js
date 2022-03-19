386ece64ff1b8cb5f8557e3f1446f4f9e46010f6
310896024@qq.com <zhanhao>
2022-03-19 21:32:29 +0800

    dev

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const glob = require("glob");
+
+const list = {};
+
+async function makeList(dirPath, list) {
+  const files = glob.sync(`${dirPath}/**/index.js`);
+  for (let file of files) {
+    const component = file.split(/[/.]/)[2];
+    list[component] = `./${file}`;
+  }
+  console.log(list);
+}
+
+makeList("components/lib", list);
+
+module.exports = {
+  entry: list,
+  output: {
+    filename: "[name].umd.js",
+    path: "dist",
+    library: "mui",
+    libraryTarget: "umd",
+  },
+};
