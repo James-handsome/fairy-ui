70cd30276097cafd1ab705e14f2d29b71a928e72
310896024@qq.com <zhanhao>
2022-03-19 23:44:20 +0800

    dev

--- examples/main.js
+++ examples/main.js
@@ -1,7 +1,5 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
 
 import "../components/css/card.less";
 import Card from "../components/lib/card/index";
@@ -11,7 +9,5 @@ Vue.use(Card);
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
   render: (h) => h(App),
 }).$mount("#app");
