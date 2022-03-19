953ec1404829dfb38bbdf06fbe8dc78b3c9d64c6
310896024@qq.com <zhanhao>
2022-03-19 22:36:46 +0800

    dev

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require("gulp");
+
+const less = require("gulp-less");
+const minifyCSS = require("gulp-minify-css");
+
+gulp.task("less", async function () {
+  return gulp
+    .src("components/css/**/*.less")
+    .pipe(less())
+    .pipe(minifyCSS())
+    .pipe(gulp.dest("dist/css"));
+});
