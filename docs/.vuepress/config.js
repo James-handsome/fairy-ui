2d38ec2b6322d9629f92c1b697a4493a3b71431f
310896024@qq.com <zhanhao>
2022-03-20 11:24:59 +0800

    dev

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'fairy-ui',
+  description: '简洁,高效,快速',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'github', link: 'https://github.com/James-handsome/fairy-ui' },
+    ],
+    sidebar: [
+      '/',
+      'componentDocs/card'
+    ]
+  }
+}
