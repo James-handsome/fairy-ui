aa9726c7dab2d673534409f095762d4b24e5daa1
310896024@qq.com <zhanhao>
2022-03-20 20:08:26 +0800

    dev

--- components/utils/csv.js
+++ components/utils/csv.js
@@ -1,63 +1,55 @@
-/*
-  inspired by https://www.npmjs.com/package/react-csv-downloader
-  now removed from Github
-*/
+// https://github.com/Terminux/react-csv-downloader/blob/master/src/lib/csv.js
 
-const newLine = "\r\n";
-const appendLine = (content, row, { separator, quoted }) => {
-  const line = row.map((data) => {
-    if (!quoted) return data;
-    // quote data
-    data = typeof data === "string" ? data.replace(/"/g, '"') : data;
-    return `"${data}"`;
-  });
-  content.push(line.join(separator));
-};
+const newLine = '\r\n';
 
-const defaults = {
-  separator: ",",
-  quoted: false,
-};
+export default function csv(columns, datas, separator = ',', noHeader = false) {
+    let columnOrder;
+    const content = [];
+    const column = [];
 
-export default function csv(columns, datas, options, noHeader = false) {
-  options = Object.assign({}, defaults, options);
-  let columnOrder;
-  const content = [];
-  const column = [];
+    if (columns) {
+        columnOrder = columns.map(v => {
+            if (typeof v === 'string') {
+                return v;
+            }
+            if (!noHeader) {
+                column.push((typeof v.title !== 'undefined') ? v.title : v.key);
+            }
+            return v.key;
+        });
+        if (column.length > 0) {
+            content.push(column.join(separator));
+        }
+    } else {
+        columnOrder = [];
+        datas.forEach(v => {
+            if (!Array.isArray(v)) {
+                columnOrder = columnOrder.concat(Object.keys(v));
+            }
+        });
+        if (columnOrder.length > 0) {
+            columnOrder = columnOrder.filter((value, index, self) => self.indexOf(value) === index);
 
-  if (columns) {
-    columnOrder = columns.map((v) => {
-      if (typeof v === "string") return v;
-      if (!noHeader) {
-        column.push(typeof v.title !== "undefined" ? v.title : v.key);
-      }
-      return v.key;
-    });
-    if (column.length > 0) appendLine(content, column, options);
-  } else {
-    columnOrder = [];
-    datas.forEach((v) => {
-      if (!Array.isArray(v)) {
-        columnOrder = columnOrder.concat(Object.keys(v));
-      }
-    });
-    if (columnOrder.length > 0) {
-      columnOrder = columnOrder.filter(
-        (value, index, self) => self.indexOf(value) === index
-      );
-      if (!noHeader) appendLine(content, columnOrder, options);
+            if (!noHeader) {
+                content.push(columnOrder.join(separator));
+            }
+        }
     }
-  }
 
-  if (Array.isArray(datas)) {
-    datas.forEach((row) => {
-      if (!Array.isArray(row)) {
-        row = columnOrder.map((k) =>
-          typeof row[k] !== "undefined" ? row[k] : ""
-        );
-      }
-      appendLine(content, row, options);
-    });
-  }
-  return content.join(newLine);
+    if (Array.isArray(datas)) {
+        datas.map(v => {
+            if (Array.isArray(v)) {
+                return v;
+            }
+            return columnOrder.map(k => {
+                if (typeof v[k] !== 'undefined') {
+                    return v[k];
+                }
+                return '';
+            });
+        }).forEach(v => {
+            content.push(v.join(separator));
+        });
+    }
+    return content.join(newLine);
 }
