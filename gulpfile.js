61198ff3db24804b2571a4999ec1784dcc509072
310896024@qq.com <zhanhao>
2022-03-20 23:48:27 +0800

    dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const gulp = require("gulp");
-
 const less = require("gulp-less");
 const minifyCSS = require("gulp-minify-css");
 
@@ -10,3 +9,15 @@ gulp.task("less", async function () {
     .pipe(minifyCSS())
     .pipe(gulp.dest("dist/css"));
 });
+
+// 拷贝字体文件
+gulp.task("fonts", async function () {
+  return gulp
+    .src("./components/styles/common/iconfont/fonts/*.*")
+    .pipe(gulp.dest("dist/css/fonts"));
+});
+
+gulp.task(
+  "default",
+  gulp.parallel("less", "fonts", (done) => done())
+);
