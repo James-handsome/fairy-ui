69caa1d8fa18b9bc7ff83ec4ac75f563dd5e655a
310896024@qq.com <zhanhao>
2022-03-27 21:47:38 +0800

    dev

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     'prettier/prettier': [
-      'error',
+      'warn',
       {
         endOfLine: 'auto',
       },
