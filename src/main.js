4cef38f0111e1cb3ffa21d130e6ac1540c8a2e89
310896024@qq.com <zhanhao>
2022-04-01 00:19:01 +0800

    dev

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from './router'
 
-import "../components/styles/index.less";
-import fairyUi from "../components/lib/index";
+import "../packages/styles/index.less";
+import fairyUi from "../packages/index";
 
 Vue.use(fairyUi);
 
