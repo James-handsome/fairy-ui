5d4191ffb9e25dd7e764de274f7de4f7bd4199ed
310896024@qq.com <zhanhao>
2022-04-04 22:23:02 +0800

    d

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  pages: {
+    index: {
+      entry: "examples/main.js",
+      template: "public/index.html",
+      filename: "index.html",
+    },
+  },
   css: {
     loaderOptions: {
       less: {
