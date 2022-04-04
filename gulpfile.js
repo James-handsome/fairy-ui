4c1b36705f56bbd53b624047cfeb8b0ea32fb7ec
310896024@qq.com <zhanhao>
2022-04-04 22:49:02 +0800

    d

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 const gulp = require("gulp");
 const less = require("gulp-less");
-const minifyCss = require("gulp-minify-css");
-const rename = require("gulp-rename");
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
 
-
-gulp.task("less", function () {
-  gulp
-    .src("./packages/styles/index.less")
+// 打包css
+gulp.task("less", async function () {
+  return gulp
+    .src("./packages/styles/*.less")
     .pipe(less())
-    .pipe(minifyCss())
-    .pipe(rename("fairy-ui.css"))
+    .pipe(cleanCSS())
+    .pipe(rename('fairy-ui.css'))
     .pipe(gulp.dest("lib/styles"));
 });
 
 // 拷贝字体文件
-gulp.task("fonts", function () {
-  gulp
+gulp.task("fonts", async function () {
+  return gulp
     .src("./packages/styles/common/iconfont/fonts/*.*")
     .pipe(gulp.dest("lib/styles/fonts"));
 });
