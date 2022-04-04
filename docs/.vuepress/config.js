e80cf7069fc460df69b66e950b0937e82df3ff44
310896024@qq.com <zhanhao>
2022-04-04 21:29:46 +0800

    d

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -42,30 +42,13 @@ module.exports = {
           "/component/Grid",
           "/component/Card",
           "/component/Button",
+          "/component/Icon",
         ],
       },
      ],
     },
     head:[
       ['meta', { name: 'keywords', content: 'fairy-ui 前端,vue,组件库,vue组件库,UI框架,前端UI框架' }],
-       // <!--百度站长自动推送-->
-      [
-        "script",
-        {},
-        `
-        (function(){
-          var bp = document.createElement('script');
-          var curProtocol = window.location.protocol.split(':')[0];
-          if (curProtocol === 'https') {
-              bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
-          }
-          else {
-              bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-          }
-          var s = document.getElementsByTagName("script")[0];
-          s.parentNode.insertBefore(bp, s);
-         })();`
-      ]
     ]
   }
 }
---
+++ docs/component/Icon.md
@@ -0,0 +1,16 @@
+### Icon 图标
+
+#### 如何使用
+
+使用`<Icon />`组件，指定图标对应的type属性，示例代码
+
+```vue
+<Icon type="checkmark" />
+```
+
+#### 渲染后为
+
+
+```html
+<i class="ivu-icon ivu-icon-checkmark"></i>
+```
