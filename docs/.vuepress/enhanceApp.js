caf201598e7d37288c3ca430bb3e77a22a2fbe67
310896024@qq.com <zhanhao>
2022-04-04 23:44:18 +0800

    d

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -3,10 +3,8 @@ import './common.less';
 import '../../lib/fairyUi.css'
 import fairyUi from '../../lib/fairyUi.umd.js'
 
-export default ({
+export default async ({
   Vue,
 }) => {
-  if(typeof process === "undefined"){
-    Vue.use(fairyUi)
-  }
+  Vue.use(fairyUi)
 }
